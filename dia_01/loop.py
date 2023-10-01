6c927b41b3e7906a0f834b813e2c6040759d605f
Diego Broetto <diego.broetto1@gmail.com>
2023-10-01 18:14:52 -0300

    primeiros códigos

---
+++ dia_01/loop.py
@@ -0,0 +1,8 @@
+nome = "Diego "
+lista = [1, 2, 3, 5]
+numeros = range(1,20)
+
+
+
+for num in range(1, 10):
+    print(f"{nome * num}")
---
+++ dia_01/tipo_numerico.py
@@ -0,0 +1,9 @@
+num1 = int(input("Digite um numero: "))
+num2 = int(input("Digite outro numero:"))
+
+if num1 > num2:
+    print(f"O {num1} é maior que {num2}")
+else:
+    print(f"O {num2} é maior que {num1}")
+
+
