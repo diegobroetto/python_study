37f5499830d1e5791ffcb2c9b088905779dbb94f
Diego Broetto <diego.broetto1@gmail.com>
2023-11-26 15:38:28 -0300

    Aprendendo a usar OrderedDict

--- dia_05/ordered_dict.py
+++ dia_05/ordered_dict.py
@@ -1,11 +1,17 @@
 """
 -Módulo Collections: Ordered Dict
 
+Em um dicionários a ordem de inserção não é garantida.
+
+OrderedDict -> É um dicionário que garante a ordem de inserção dos elementos
+
 """
 
-#em um dicionários a ordem de inserção não é garantida.
-dicionario = {'a': 1, 'b': 2, 'c': 3, 'd': 4}
-
-print(dicionario)
+from collections import OrderedDict
 
 
+dicionario = OrderedDict({'a': 1, 'c': 2, 'b': 4, 'd': 3, 'e': 5, 'f': 6,})
+
+
+for chave, valor in dicionario.items():
+    print(f'chave={chave} valor={valor}')
